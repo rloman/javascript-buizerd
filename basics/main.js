4ee155cd8d8fcd162522b6c867995c62334e4f6b
Raymond Loman <raymond@carpago.nl>
2019-05-28 15:15:16 +0200

    Init

---
+++ basics/main.js
@@ -0,0 +1,43 @@
+"use strict"
+
+let a = 45;
+let b = 15;
+
+
+
+
+a = a + b;
+
+console.log(a);
+
+
+// this function returns the ggd (grootste gemene deler)
+function ggd(a, b) {
+
+}
+
+
+// dit is mijn leeftijd
+let myAge = 50;
+
+/*
+
+dit is een ander zijn leeftijd
+
+
+*/
+
+let age2 = 45;  // einde van de regel is ook goed!!!
+
+let test;
+if (true) {
+    test = true; // use "var" instead of "let"
+}
+
+alert(test); // true, the variable lives after if
+
+let UserName = "Jan"; // goed maar niet volgens conventies
+let userName = "Piet";
+
+
+let deKameelMetTweeBulten = "Poedel";
