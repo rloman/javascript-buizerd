9b9fb1f6c9f48b4a1d309410f0657a46decb3c82
Raymond Loman <raymond@carpago.nl>
2019-05-28 15:39:54 +0200

    Some improve

--- basics/main.js
+++ basics/main.js
@@ -3,41 +3,31 @@
 let a = 45;
 let b = 15;
 
-
-
-
 a = a + b;
 
 console.log(a);
-
-
-// this function returns the ggd (grootste gemene deler)
-function ggd(a, b) {
-
-}
 
 
 // dit is mijn leeftijd
 let myAge = 50;
 
 /*
-
 dit is een ander zijn leeftijd
-
-
 */
-
 let age2 = 45;  // einde van de regel is ook goed!!!
 
-let test;
 if (true) {
-    test = true; // use "var" instead of "let"
+    vartest = true; // bad practice
 }
 
 alert(test); // true, the variable lives after if
 
+let test2;
+if(true) {
+    test2 = true; // good practice
+}
+
 let UserName = "Jan"; // goed maar niet volgens conventies
 let userName = "Piet";
-
 
 let deKameelMetTweeBulten = "Poedel";
