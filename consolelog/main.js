96f5dc1a8a9da70cf48005b6255ef49676f06e44
Raymond Loman <raymond@carpago.nl>
2019-06-18 10:18:17 +0200

    Add function demo

---
+++ consolelog/main.js
@@ -0,0 +1,35 @@
+function square(x) {
+    let result = x**2;
+
+    return result;
+}
+
+function bad(x) {
+    let result = x+2;
+
+    return result;
+}
+
+function getName() {
+    let result = "Ronnie";
+
+    return result;
+}
+
+let vier = square(2);
+
+let resultOfConsoleLogFunction = console.log(vier);
+
+console.log(resultOfConsoleLogFunction);
+
+console.log(bad(4));
+
+let nameOfSomeone = getName();
+
+console.log(nameOfSomeone);
+
+
+console.log(getName());
+console.log(getName());
+console.log(getName());
+console.log(getName());
